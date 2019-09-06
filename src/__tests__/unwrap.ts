c78a52bf3ab260cfa40436500132713635e76990
DomParfitt <dominicparfitt@economist.com>
2019-09-06 11:40:48 +0100

    Added test stubs

---
+++ src/__tests__/unwrap.ts
@@ -0,0 +1,33 @@
+describe('unwrap', () => {
+  it('unwraps the event when it is of the expected type', () => {
+    fail();
+  });
+
+  it('unwraps the event when it comes directly from SQS', () => {
+    fail();
+  });
+
+  it('unwraps the event when it comes from SNS via SQS', () => {
+    fail();
+  });
+});
+
+describe('unwrapAll', () => {
+  it('unwraps the event to an array of the expected type when it is of the expected type', () => {
+    fail();
+  });
+});
+
+describe('unwrapFirst', () => {
+  it('unwraps the event when it is of the expected type', () => {
+    fail();
+  });
+
+  it('unwraps the event when it comes directly from SQS', () => {
+    fail();
+  });
+
+  it('unwraps the event when it comes from SNS via SQS', () => {
+    fail();
+  });
+});
